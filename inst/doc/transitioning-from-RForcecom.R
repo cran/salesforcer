a49acf246066a3811f6400145c8b52223c1deea8
Steven M. Mortimer <mortimer.steven.m@gmail.com>
2021-07-06 08:10:03 +0000

    version 1.0.0

--- inst/doc/transitioning-from-RForcecom.R
+++ inst/doc/transitioning-from-RForcecom.R
@@ -8,7 +8,11 @@ knitr::opts_chunk$set(
 )
 options(tibble.print_min = 5L, tibble.print_max = 5L)
 
-## ----auth, include = FALSE----------------------------------------------------
+## ----auth, eval=FALSE---------------------------------------------------------
+#  library(dplyr, warn.conflicts = FALSE)
+#  library(salesforcer)
+
+## ----auth-background, include = FALSE-----------------------------------------
 library(dplyr, warn.conflicts = FALSE)
 library(salesforcer)
 
@@ -22,17 +26,14 @@ sf_auth(username = username,
 
 ## ---- warning=FALSE-----------------------------------------------------------
 # the RForcecom way
-session1 <- RForcecom::rforcecom.login(username, paste0(password, security_token), 
-                                       apiVersion=getOption("salesforcer.api_version"))
-session1['sessionID'] <- "{MASKED}"
-session1
-
+# RForcecom::rforcecom.login(username, paste0(password, security_token), 
+#                            apiVersion=getOption("salesforcer.api_version"))
 # replicated in salesforcer package
-session2 <- salesforcer::rforcecom.login(username, 
+session <- salesforcer::rforcecom.login(username, 
                                          paste0(password, security_token), 
                                          apiVersion = getOption("salesforcer.api_version"))
-session2['sessionID'] <- "{MASKED}"
-session2
+session['sessionID'] <- "{MASKED}"
+session
 
 ## ---- include=FALSE-----------------------------------------------------------
 # keep using the session, just rename it to "session"
@@ -45,27 +46,25 @@ object <- "Contact"
 fields <- c(FirstName="Test", LastName="Contact-Create-Compatibility")
 
 # the RForcecom way
-result1 <- RForcecom::rforcecom.create(session, objectName=object, fields)
-result1
+# RForcecom::rforcecom.create(session, objectName=object, fields)
 
 # replicated in salesforcer package
-result2 <- salesforcer::rforcecom.create(session, objectName=object, fields)
-result2
+result <- salesforcer::rforcecom.create(session, objectName=object, fields)
+result
 
 ## ---- warning=FALSE-----------------------------------------------------------
 n <- 2
 new_contacts <- tibble(FirstName = rep("Test", n),
                        LastName = paste0("Contact-Create-", 1:n))
 
-# the RForcecom way
-rforcecom_results <- NULL
-for(i in 1:nrow(new_contacts)){
-  temp <- RForcecom::rforcecom.create(session, 
-                                      objectName = "Contact", 
-                                      fields = unlist(slice(new_contacts,i)))
-  rforcecom_results <- bind_rows(rforcecom_results, temp)
-}
-rforcecom_results
+# the RForcecom way (requires a loop)
+# rforcecom_results <- NULL
+# for(i in 1:nrow(new_contacts)){
+#   temp <- RForcecom::rforcecom.create(session, 
+#                                       objectName = "Contact", 
+#                                       fields = unlist(slice(new_contacts,i)))
+#   rforcecom_results <- bind_rows(rforcecom_results, temp)
+# }
 
 # the better way in salesforcer to do multiple records
 salesforcer_results <- sf_create(new_contacts, object_name="Contact")
@@ -75,12 +74,11 @@ salesforcer_results
 this_soql <- "SELECT Id, Email FROM Contact LIMIT 5"
 
 # the RForcecom way
-result1 <- RForcecom::rforcecom.query(session, soqlQuery = this_soql)
-result1
+# RForcecom::rforcecom.query(session, soqlQuery = this_soql)
 
 # replicated in salesforcer package
-result2 <- salesforcer::rforcecom.query(session, soqlQuery = this_soql)
-result2
+result <- salesforcer::rforcecom.query(session, soqlQuery = this_soql)
+result
 
 # the better way in salesforcer to query
 salesforcer_results <- sf_query(this_soql)
@@ -88,12 +86,12 @@ salesforcer_results
 
 ## ---- warning=FALSE-----------------------------------------------------------
 # the RForcecom way
-result1 <- RForcecom::rforcecom.getObjectDescription(session, objectName='Account')
+# RForcecom::rforcecom.getObjectDescription(session, objectName='Account')
 
 # backwards compatible in the salesforcer package
-result2 <- salesforcer::rforcecom.getObjectDescription(session, objectName='Account')
+result <- salesforcer::rforcecom.getObjectDescription(session, objectName='Account')
 
 # the better way in salesforcer to get object fields
-result3 <- salesforcer::sf_describe_object_fields('Account')
-result3
+result2 <- salesforcer::sf_describe_object_fields('Account')
+result2
 
